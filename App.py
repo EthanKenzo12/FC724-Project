2dc2d92c76484d098fa59ca267f09046315c5320
EthanKenzo12 <148215779+EthanKenzo12@users.noreply.github.com>
2024-03-18 20:37:44 +0000

    Refactor 1
    Imported necessary external files.
    - Created class SurveyForm, inheriting from FlaskForm.
    - Added 3 attributes to the SurveyForm.
    - StuNum Notably has max character length of 7

--- App.py
+++ App.py
@@ -1,1 +1,10 @@
-from flask import wtforms
+from flask_wtf import FlaskForm
+from wtforms import StringField, RadioField, SelectField, TextAreaField, IntegerField, EmailField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class SurveyForm(FlaskForm):
+    FName = StringField('First Name', validators=[DataRequired])
+    LName = StringField('Last Name', validators=[DataRequired])
+    StuNum = StringField('Student Number', validators=[DataRequired, Length(max=7)])
+
