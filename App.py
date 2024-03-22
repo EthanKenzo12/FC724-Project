0f721003ba452f1a8f5e1b483ab7da4c6c4a14b2
EthanKenzo12 <148215779+EthanKenzo12@users.noreply.github.com>
2024-03-22 14:55:36 +0000

    Moved Images Folder.
    Moved Images Folder, to new folder titled Static

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, flash, Flask
-from Survey import app
 from Survey import SurveyForm
+
+app = Flask(__name__)
 
 
 @app.route('/survey', methods=['GET', 'POST'])
@@ -10,4 +11,4 @@ def survey():
         # Process the data
         flash('Survey submitted successfully!')
         return redirect(url_for('some_function'))
-    return render_template('(Survey).html', form=form)
+    return render_template('Page%3%(Survey).html', form=form)
