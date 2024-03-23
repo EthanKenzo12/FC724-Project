484b5449ea1f513f8750898187a16d926f93907c
EthanKenzo12 <148215779+EthanKenzo12@users.noreply.github.com>
2024-03-23 23:45:12 +0000

    Update app.py
    Updated app.py with the with open function:
    
    Function stores the data that is recorded on the server side as a text file that is automatically printed onto the Data.txt file.

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-from flask import render_template, redirect, url_for, flash, Flask, jsonify
+from flask import render_template, redirect, url_for, flash, Flask
 from survey import SurveyForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'some-random-secret-key'
+app.config['SECRET_KEY'] = 'GIC'
 
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = SurveyForm()
     if form.validate_on_submit():
-        # Process the data
+        with open("Data.txt", "a") as l:
+            l.write(f'FName: {form.FName.data}\n')
+            l.write(f'LName: {form.LName.data}\n')
+            l.write(f'SNum: {form.SNum.data}\n')
+            l.write(f'Gender: {form.Gender.data}\n')
+            l.write(f'Intake: {form.Intake.data}\n')
+            l.write(f'Email: {form.Email.data}\n')
+            l.write(f'Course: {form.Course.data}\n')
+            l.write(f'Overall: {form.Overall.data}\n')
+            l.write(f'Grade_1:{form.Grade_1.data}\n')
+            l.write(f'Grade_2:{form.Grade_2.data}\n')
+            l.write(f'Grade_3:{form.Grade_3.data}\n')
+            l.write(f'Grade_4:{form.Grade_4.data}\n')
+            l.write(f'Experience: {form.Experience.data}\n')
+            l.write(f'Why_1:{form.Why_1.data}\n')
+            l.write(f'Suggestions:{form.Suggestions.data}\n')
+            l.write(f'--------line break--------\n')
+
         flash('Survey submitted successfully!')
-        return redirect(url_for('survey'))
+        return redirect(url_for('form'))
     return render_template("survey.html", form=form)
 
 
