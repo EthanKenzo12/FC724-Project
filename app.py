bab48d84db13f47cc9bd0cc21bdeba98f5edac09
EthanKenzo12 <148215779+EthanKenzo12@users.noreply.github.com>
2024-03-24 15:58:30 +0000

    Commenting
    Added Comments to app.py file for additional clarity of the code and their associated functions.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import render_template, redirect, url_for, flash, Flask
 from survey import SurveyForm
 
+# creation of secret key for session security and CSRF protection
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'GIC'
 
-
+# routed app to /form when url: http://127.0.0.1:5000/form is accessed, associated html page is accessed
+# user will be directed to the survey for directly
+# creation of form function,
+# use of with open such that data input on the server-side survey is saved as text in Data.txt
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = SurveyForm()
@@ -27,19 +31,25 @@ def form():
             l.write(f'Suggestions:{form.Suggestions.data}\n')
             l.write(f'--------line break--------\n')
 
+        # use of flash to create flash message to inform user of successful survey submission
         flash('Survey submitted successfully!')
+        # use of redirect to redirect user to the survey page after survey is submitted
         return redirect(url_for('form'))
+    # use of render template to access survey.html, HTML page
     return render_template("survey.html", form=form)
 
 
+# routed app to /home when url: http://127.0.0.1:5000 is accessed, associated html page is accessed
+# as the default page is the home page default localhost website will be http://127.0.0.1:5000
 @app.route('/')
-@app.route('/home')
 def home():
+    # use of render template to access Homepage.html, HTML page
     return render_template('Homepage.html')
 
-
+# routed app to /about when url: http://127.0.0.1:5000/about is accessed, associated html page is accessed
 @app.route('/about')
 def about():
+    # use of render template to access About.html, HTML page
     return render_template('About.html')
 
 
